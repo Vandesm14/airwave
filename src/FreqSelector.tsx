e0652aa122da62abefdd1060ab1dc86186db20bc
Shane <vandesm14@gmail.com>
2024-08-16 12:53:18 -0400

    store standby frequency

--- src/FreqSelector.tsx
+++ src/FreqSelector.tsx
@@ -1,10 +1,11 @@
 import { useAtom } from 'solid-jotai';
 import { frequencyAtom } from './lib/atoms';
 import { createSignal, onMount } from 'solid-js';
+import { makePersisted } from '@solid-primitives/storage';
 
 export default function FreqSelector() {
   let [frequency, setFrequency] = useAtom(frequencyAtom);
-  let [secondary, setSecondary] = createSignal(frequency());
+  let [secondary, setSecondary] = makePersisted(createSignal(frequency()));
 
   function swap() {
     let swapFreq = frequency();
