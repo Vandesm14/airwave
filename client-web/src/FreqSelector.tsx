30b6a05f03277db74d20da95f24f86a003e66bae
Shane <vandesm14@gmail.com>
2024-09-09 13:39:26 -0400

    dual frequency input

--- client-web/src/FreqSelector.tsx
+++ client-web/src/FreqSelector.tsx
@@ -1,7 +1,8 @@
 import { useAtom } from 'solid-jotai';
 import { controlAtom, frequencyAtom, worldAtom } from './lib/atoms';
-import { createMemo, createSignal, onMount } from 'solid-js';
+import { createEffect, createMemo, createSignal, onMount } from 'solid-js';
 import { makePersisted } from '@solid-primitives/storage';
+import { Frequencies } from './lib/types';
 
 export default function FreqSelector() {
   let [frequency, setFrequency] = useAtom(frequencyAtom);
@@ -10,20 +11,41 @@ export default function FreqSelector() {
 
   let [control] = useAtom(controlAtom);
   let [airspace] = useAtom(control().airspace);
+  let [key, setKey] = createSignal<keyof Frequencies>('approach');
 
   let foundAirspace = createMemo(() =>
     world().airspaces.find((a) => a.id === airspace())
   );
 
+  function changeViaKey(key: string) {
+    setKey(key as keyof Frequencies);
+
+    if (foundAirspace()?.frequencies && key in foundAirspace()?.frequencies) {
+      setFrequency(foundAirspace()?.frequencies[key]);
+    }
+  }
+
+  function changeViaValue(value: number) {
+    setFrequency(value);
+    if (foundAirspace()?.frequencies) {
+      setKey(
+        Object.keys(foundAirspace()?.frequencies).find(
+          (k) => foundAirspace()?.frequencies[k] === value
+        ) as keyof Frequencies
+      );
+    }
+  }
+
   function swap() {
-    let swapFreq = frequency();
-    setFrequency(secondary());
-    setSecondary(swapFreq);
+    // let swapFreq = frequency();
+    // setFrequency(secondary());
+    // setSecondary(swapFreq);
   }
 
   function oninput(e: InputEvent) {
     if (e.target instanceof HTMLInputElement) {
-      setSecondary(parseFloat(e.target.value));
+      // setSecondary(parseFloat(e.target.value));
+      changeViaValue(parseFloat(e.target.value));
     }
   }
 
@@ -45,19 +67,28 @@ export default function FreqSelector() {
 
   return (
     <div id="freq-selector">
-      <input type="number" disabled value={frequency()} class="live" />
-      <input type="button" value="⬌" class="swap" onclick={swap} />
       <select
         name="frequency"
         id=""
-        onchange={(e) => setSecondary(parseFloat(e.target.value))}
+        onchange={(e) => changeViaKey(e.target.value)}
       >
         {foundAirspace()?.frequencies
-          ? Object.entries(foundAirspace()?.frequencies).map(([key, value]) => (
-              <option value={value}>{key}</option>
+          ? Object.keys(foundAirspace()?.frequencies).map((k) => (
+              <option value={k} selected={k === key()}>
+                {k}
+              </option>
             ))
           : null}
       </select>
+      <input
+        type="number"
+        value={frequency()}
+        class="live"
+        oninput={oninput}
+        onkeydown={onEnter}
+        step=".1"
+      />
+      {/* <input type="button" value="⬌" class="swap" onclick={swap} /> */}
     </div>
   );
 }
