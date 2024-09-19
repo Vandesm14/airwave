4953f5ac1f2df66390f9f7e8374daa3a5bd7c7f4
Shane <vandesm14@gmail.com>
2024-09-19 10:35:03 -0400

    add comments

--- engine/src/engine.rs
+++ engine/src/engine.rs
@@ -26,21 +26,26 @@ impl Engine {
     };
 
     for aircraft in self.aircraft.iter_mut() {
+      // Capture the previous state
       bundle.prev = aircraft.clone();
 
+      // Run through all events
       for event in self.events.iter() {
         HandleAircraftEvent::run(aircraft, event, &mut bundle);
       }
 
+      // Run through all effects
       AircraftUpdateFromTargetsEffect::run(aircraft, &mut bundle);
       AircraftUpdatePositionEffect::run(aircraft, &mut bundle);
       AircraftIsPast205Effect::run(aircraft, &mut bundle);
 
+      // Run through all actions
       for action in bundle.actions.drain(..) {
         AircraftAllActionHandler::run(aircraft, &action);
       }
     }
 
+    // Capture the left over events and actions for next time
     self.events = core::mem::take(&mut bundle.events);
     self.actions = core::mem::take(&mut bundle.actions);
   }
