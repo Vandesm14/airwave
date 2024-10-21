527d39b5329d129b00c90040f8664bb96387cc25
Shane <vandesm14@gmail.com>
2024-10-21 17:57:48 -0400

    add license to vendored package

--- client-web/src/whisper/WhisperSTT.ts
+++ client-web/src/whisper/WhisperSTT.ts
@@ -1,3 +1,31 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2023 Nitai Aharoni
+// Copyright (c) 2024 Shane Vandegrift
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+// -----------------------------------------------------------------------------
+//
+// Code taken from (with modifications):
+// https://github.com/nitaiaharoni1/whisper-speech-to-text/blob/8dfaf57bb20eadb2aa5958e8c8edd20e3ba34d57/src/WhisperSTT.ts
+
 import { RecordRTCPromisesHandler } from 'recordrtc';
 
 const AUDIO_TYPE = 'audio';
