a57107355b9702b2c8bd8c0160ca910762615e2c
Shane <vandesm14@gmail.com>
2024-10-24 20:47:15 -0400

    use custom asset server

--- serve/src/main.rs
+++ serve/src/main.rs
@@ -4,10 +4,21 @@ use core::{
 };
 use std::path::PathBuf;
 
-use axum::Router;
+use axum::{
+  extract::Path,
+  http::{HeaderValue, StatusCode},
+  response::{Html, IntoResponse, Response},
+  routing::get,
+  Router,
+};
 use clap::Parser;
+use rust_embed::Embed;
 use tokio::net::TcpListener;
-use tower_http::{cors::CorsLayer, services::ServeDir, trace::TraceLayer};
+use tower_http::{cors::CorsLayer, trace::TraceLayer};
+
+#[derive(Embed)]
+#[folder = "../client-web/dist"]
+struct Asset;
 
 #[tokio::main]
 async fn main() {
@@ -28,9 +39,9 @@ async fn main() {
   let serve = axum::serve(
     listener,
     Router::new()
-      .nest_service("/", ServeDir::new(&dir_path))
       .layer(TraceLayer::new_for_http())
-      .layer(CorsLayer::permissive()),
+      .layer(CorsLayer::permissive())
+      .route("/*path", get(my_file_server)),
   )
   .await;
 
@@ -40,6 +51,68 @@ async fn main() {
       tracing::error!("{e}");
       std::process::exit(2);
     }
+  }
+}
+
+enum MyResponse {
+  Html(String),
+  Css(String),
+  Js(String),
+}
+
+impl IntoResponse for MyResponse {
+  fn into_response(self) -> axum::response::Response {
+    match self {
+      MyResponse::Html(x) => {
+        let mut response = Response::new(x.into());
+        response
+          .headers_mut()
+          .append("content-type", HeaderValue::from_str("text/html").unwrap());
+
+        response
+      }
+      MyResponse::Css(x) => {
+        let mut response = Response::new(x.into());
+        response
+          .headers_mut()
+          .append("content-type", HeaderValue::from_str("text/css").unwrap());
+
+        response
+      }
+      MyResponse::Js(x) => {
+        let mut response = Response::new(x.into());
+        response.headers_mut().append(
+          "content-type",
+          HeaderValue::from_str("text/javascript").unwrap(),
+        );
+
+        response
+      }
+    }
+  }
+}
+
+async fn my_file_server(Path(path): Path<String>) -> impl IntoResponse {
+  match Asset::get(&path) {
+    Some(file) => {
+      // Bytes::copy_from_slice(&file.data)
+      match String::from_utf8(file.data.to_vec()) {
+        Ok(str) => {
+          if let Some(ext) = PathBuf::from(path).extension() {
+            match ext.to_str().unwrap() {
+              "html" => Ok(MyResponse::Html(str)),
+              "css" => Ok(MyResponse::Css(str)),
+              "js" => Ok(MyResponse::Js(str)),
+              _ => Err(StatusCode::IM_A_TEAPOT),
+            }
+          } else {
+            Err(StatusCode::EXPECTATION_FAILED)
+          }
+        }
+        Err(_) => Err(StatusCode::INTERNAL_SERVER_ERROR),
+      }
+    }
+    None => Err(StatusCode::NOT_FOUND),
   }
 }
 
