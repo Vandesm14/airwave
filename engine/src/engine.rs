0ec24c1a9ea3a169f4b9a086320b56f0c0afa10c
Shane <vandesm14@gmail.com>
2024-11-21 17:01:57 -0500

    match both aircraft as flying

--- engine/src/engine.rs
+++ engine/src/engine.rs
@@ -114,6 +114,10 @@ impl Engine {
         (aircraft.altitude - other_aircraft.altitude).abs();
 
       if matches!(aircraft.state, AircraftState::Flying { enroute: false, .. })
+        && matches!(
+          other_aircraft.state,
+          AircraftState::Flying { enroute: false, .. }
+        )
         && aircraft.altitude > 1000.0
         && distance <= (NAUTICALMILES_TO_FEET * 4.0).powf(2.0)
         && vertical_distance < 1000.0
