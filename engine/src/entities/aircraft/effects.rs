ce49d4753bb96a2474155715c92e7cc980bae58c
Shane <vandesm14@gmail.com>
2024-12-03 22:55:49 -0500

    add comment

--- engine/src/entities/aircraft/effects.rs
+++ engine/src/entities/aircraft/effects.rs
@@ -361,6 +361,8 @@ impl AircraftEffect for AircraftUpdateTaxiingEffect {
         if let NodeBehavior::Park = current.behavior {
           aircraft.state = AircraftState::Parked {
             at: current.clone(),
+            // Only become inactive if we are arriving at the player's airspace.
+            // If we are departing, keep us as active.
             active: aircraft.flight_plan.arriving != bundle.world.airspace.id,
           };
         }
