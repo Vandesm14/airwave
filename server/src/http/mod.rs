5db9abe139efcddfe7711e70da4cbf48cceeeb15
Shane <vandesm14@gmail.com>
2024-12-05 16:28:32 -0500

    fix static file serving

--- server/src/http/mod.rs
+++ server/src/http/mod.rs
@@ -27,31 +27,32 @@ pub async fn run(
   openai_api_key: Arc<str>,
 ) {
   let cors = CorsLayer::very_permissive();
-  let app = Router::new().nest(
-    "/api",
-    Router::new()
-      .route("/", get(|| async { "Airwave API is active." }))
-      // Misc
-      .route("/pause", post(post_pause))
-      .route("/ping", get(ping_pong))
-      // Comms
-      .route("/comms/text", post(comms_text))
-      .route("/comms/voice", post(comms_voice))
-      // Aircraft
-      .route("/game/aircraft", get(get_aircraft))
-      .route("/game/aircraft/:id", get(get_one_aircraft))
-      // Flights
-      .route("/game/flights", get(get_flights))
-      .route("/game/flight", post(create_flight))
-      .route("/game/flight/:id", delete(delete_flight))
-      // State
-      .route("/messages", get(get_messages))
-      .route("/world", get(get_world))
-      .route("/game/points", get(get_points))
-      .nest_service("/static", ServeDir::new("static"))
-      .with_state(AppState::new(get_sender, post_sender, openai_api_key))
-      .layer(cors),
-  );
+  let app = Router::new()
+    .nest(
+      "/api",
+      Router::new()
+        .route("/", get(|| async { "Airwave API is active." }))
+        // Misc
+        .route("/pause", post(post_pause))
+        .route("/ping", get(ping_pong))
+        // Comms
+        .route("/comms/text", post(comms_text))
+        .route("/comms/voice", post(comms_voice))
+        // Aircraft
+        .route("/game/aircraft", get(get_aircraft))
+        .route("/game/aircraft/:id", get(get_one_aircraft))
+        // Flights
+        .route("/game/flights", get(get_flights))
+        .route("/game/flight", post(create_flight))
+        .route("/game/flight/:id", delete(delete_flight))
+        // State
+        .route("/messages", get(get_messages))
+        .route("/world", get(get_world))
+        .route("/game/points", get(get_points))
+        .with_state(AppState::new(get_sender, post_sender, openai_api_key))
+        .layer(cors),
+    )
+    .nest_service("/static", ServeDir::new("static"));
 
   let listener = tokio::net::TcpListener::bind(address).await.unwrap();
   tracing::info!("Listening on {address}");
