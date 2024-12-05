fd39835a62acbc59a01663e23e20924a8f73e659
Shane <vandesm14@gmail.com>
2024-12-05 16:28:38 -0500

    play audio files in the frontend

--- client-web/src/Chatbox.tsx
+++ client-web/src/Chatbox.tsx
@@ -8,7 +8,7 @@ import {
 import { createEffect, createSignal, onMount, onCleanup } from 'solid-js';
 import { RadioMessage } from './lib/types';
 import { useStorageAtom } from './lib/hooks';
-import { useMessages } from './lib/api';
+import { baseAPIPath, useMessages } from './lib/api';
 
 export default function Chatbox({
   sendMessage,
@@ -24,46 +24,23 @@ export default function Chatbox({
   let [showAll, setShowAll] = createSignal(false);
   let [text, setText] = createSignal('');
   let [lastRead, setLastRead] = createSignal(Date.now() / 1000);
-  let [voices, setVoices] = createSignal<
-    Map<String, { rate: number; pitch: number }>
-  >(new Map());
+  const [audio] = createSignal(new Audio());
+  let [queue, setQueue] = createSignal<Array<RadioMessage>>([]);
   const messages = useMessages();
 
-  function randBetween(min: number, max: number) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  }
-
-  function speak(message: RadioMessage) {
-    if (
-      useTTS() &&
-      'speechSynthesis' in window &&
-      frequency() === message.frequency
-    ) {
-      if (window.speechSynthesis.speaking || isRecording()) {
-        setTimeout(() => speak(message), 500);
-      } else {
-        const utterance = new SpeechSynthesisUtterance(
-          message.reply.replace(/[0-9]/g, '$& ')
-        );
-
-        if (voices().has(message.id)) {
-          utterance.rate = voices().get(message.id)!.rate;
-          utterance.pitch = voices().get(message.id)!.pitch;
-        } else {
-          const voice = {
-            rate: (1.0 * randBetween(100, 110)) / 100,
-            pitch: (1.3 * randBetween(80, 115)) / 100,
-          };
-          setVoices((voices) => voices.set(message.id, voice));
-          utterance.rate = voice.rate;
-          utterance.pitch = voice.pitch;
+  function play() {
+    if (useTTS()) {
+      if (audio().paused) {
+        if (queue().length > 0) {
+          audio().addEventListener('ended', play, { once: true });
         }
 
-        utterance.volume = 0.01;
-        window.speechSynthesis.speak(utterance);
+        let message = queue().pop();
+        if (message) {
+          audio().src = `${baseAPIPath}/static/replies/${message?.created.secs}.ogg`;
+          audio().play();
+        }
       }
-    } else {
-      console.log("Sorry, your browser doesn't support text to speech!");
     }
   }
 
@@ -71,7 +48,8 @@ export default function Chatbox({
     for (const message of messages.data) {
       if (message.created.secs > lastRead() && message.id !== 'ATC') {
         if (message.reply !== '') {
-          speak(message);
+          setQueue((q) => [message, ...q]);
+          play();
         } else {
           setSelectedAircraft(message.id);
         }
