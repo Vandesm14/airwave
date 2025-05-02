08d6ae5469dfc992aa3e9ee95cbbd2588bd8043f
Shane <vandesm14@gmail.com>
2025-05-02 17:26:33 -0400

    use lua

--- editor/src/watcher.rs
+++ editor/src/watcher.rs
@@ -1,7 +1,9 @@
 use std::path::PathBuf;
 
 use clap::Parser;
-use rhai::{Engine, EvalAltResult};
+use mlua::{Lua, LuaSerdeExt, Result};
+
+use engine::entities::airport::{Gate, Runway, Taxiway, Terminal};
 
 /// View and edit an Airwave world file
 #[derive(Parser, Debug)]
@@ -11,7 +13,7 @@ struct Cli {
   file: PathBuf,
 }
 
-pub fn main() -> Result<(), Box<EvalAltResult>> {
+pub fn main() -> Result<()> {
   let args = Cli::parse();
   let script = if let Ok(script) = std::fs::read_to_string(&args.file) {
     script
@@ -20,12 +22,13 @@ pub fn main() -> Result<(), Box<EvalAltResult>> {
     std::process::exit(1);
   };
 
-  // Create an 'Engine'
-  let engine = Engine::new();
+  let lua = Lua::new();
+  let globals = lua.globals();
 
-  // Run the script - prints "42"
-  engine.run(&script)?;
+  // Create Taxiway struct from a Lua table
+  let taxiway: Taxiway = lua.from_value(lua.load(script).eval()?)?;
 
-  // Done!
+  println!("{taxiway:?}");
+
   Ok(())
 }
