dce5ee46fd2c4d405c8103056174f524934ac367
Shane <vandesm14@gmail.com>
2025-05-03 23:11:36 -0400

    fix default airport

--- assets/airports/default.lua
+++ assets/airports/default.lua
@@ -1,450 +1,203 @@
-local airport = {
-  id = "KSFO",
+local TAXIWAY_DISTANCE = 400
+
+-- Runways
+local runway13 = runway({
+  id      = "13",
+  start   = { -1500, 0 },
+  heading = 135.0,
+  length  = 7000.0,
+})
+
+local runway22 = runway({
+  id      = "22",
+  start   = { 1500, 0 },
+  heading = 225.0,
+  length  = 7000.0,
+})
+
+-- Taxiways A–D around runway 22
+local taxiwayA = taxiway({
+  id = "A",
+  a  = vec2_from(runway22.start):move(runway22.heading + 90, TAXIWAY_DISTANCE):into(),
+  b  = vec2_from(runway22.start):move(runway22.heading, runway22.length):move(runway22.heading + 90, TAXIWAY_DISTANCE)
+      :into(),
+})
+
+local taxiwayB = taxiway({
+  id = "B",
+  a  = vec2_from(runway22.start):move(runway22.heading - 90, TAXIWAY_DISTANCE):into(),
+  b  = vec2_from(runway22.start):move(runway22.heading, runway22.length):move(runway22.heading - 90, TAXIWAY_DISTANCE)
+      :into(),
+})
+
+-- Taxiways C–D around runway 13
+local taxiwayC = taxiway({
+  id = "C",
+  a  = vec2_from(runway13.start):move(runway13.heading + 90, TAXIWAY_DISTANCE):into(),
+  b  = vec2_from(runway13.start):move(runway13.heading, runway13.length):move(runway13.heading + 90, TAXIWAY_DISTANCE)
+      :into(),
+})
+
+local taxiwayD = taxiway({
+  id = "D",
+  a  = vec2_from(runway13.start):move(runway13.heading - 90, TAXIWAY_DISTANCE):into(),
+  b  = vec2_from(runway13.start):move(runway13.heading, runway13.length):move(runway13.heading - 90, TAXIWAY_DISTANCE)
+      :into(),
+})
+
+-- E1–E4 as lerps between A & B
+local taxiwayE1 = taxiway({
+  id = "E1",
+  a  = vec2_from(taxiwayA.b):lerp(vec2_from(taxiwayA.a), 1.0):into(),
+  b  = vec2_from(taxiwayB.b):lerp(vec2_from(taxiwayB.a), 1.0):into(),
+})
+
+local taxiwayE2 = taxiway({
+  id = "E2",
+  a  = vec2_from(taxiwayA.b):lerp(vec2_from(taxiwayA.a), 0.5):into(),
+  b  = vec2_from(taxiwayB.b):lerp(vec2_from(taxiwayB.a), 0.5):into(),
+})
+
+local taxiwayE3 = taxiway({
+  id = "E3",
+  a  = vec2_from(taxiwayA.b):lerp(vec2_from(taxiwayA.a), 0.25):into(),
+  b  = vec2_from(taxiwayB.b):lerp(vec2_from(taxiwayB.a), 0.25):into(),
+})
+
+local taxiwayE4 = taxiway({
+  id = "E4",
+  a  = vec2_from(taxiwayA.b):lerp(vec2_from(taxiwayA.a), 0.0):into(),
+  b  = vec2_from(taxiwayB.b):lerp(vec2_from(taxiwayB.a), 0.0):into(),
+})
+
+-- F1–F4 as lerps between C & D
+local taxiwayF1 = taxiway({
+  id = "F1",
+  a  = vec2_from(taxiwayC.b):lerp(vec2_from(taxiwayC.a), 1.0):into(),
+  b  = vec2_from(taxiwayD.b):lerp(vec2_from(taxiwayD.a), 1.0):into(),
+})
+
+local taxiwayF2 = taxiway({
+  id = "F2",
+  a  = vec2_from(taxiwayC.b):lerp(vec2_from(taxiwayC.a), 0.5):into(),
+  b  = vec2_from(taxiwayD.b):lerp(vec2_from(taxiwayD.a), 0.5):into(),
+})
+
+local taxiwayF3 = taxiway({
+  id = "F3",
+  a  = vec2_from(taxiwayC.b):lerp(vec2_from(taxiwayC.a), 0.25):into(),
+  b  = vec2_from(taxiwayD.b):lerp(vec2_from(taxiwayD.a), 0.25):into(),
+})
+
+local taxiwayF4 = taxiway({
+  id = "F4",
+  a  = vec2_from(taxiwayC.b):lerp(vec2_from(taxiwayC.a), 0.0):into(),
+  b  = vec2_from(taxiwayD.b):lerp(vec2_from(taxiwayD.a), 0.0):into(),
+})
+
+local TERMINAL_SIZE = 2730
+
+-- Terminal A
+local terminalA = terminal({
+  id    = "A",
+  a     = vec2_from(taxiwayE4.b):move(runway13.heading, -1):into(),
+  b     = vec2_from(taxiwayE3.b):move(runway13.heading, -1):into(),
+  c     = vec2_from(taxiwayE3.b):move(runway13.heading, TERMINAL_SIZE):into(),
+  d     = vec2_from(taxiwayE4.b):move(runway13.heading, TERMINAL_SIZE):into(),
+  gates = {},
+  apron = {
+    vec2_from(taxiwayE4.b):lerp(vec2_from(taxiwayE3.b), 0.5):into(),
+    vec2_from(taxiwayF3.b):lerp(vec2_from(taxiwayF4.b), 0.5):into(),
+  },
+})
+terminalA.apron = {
+  vec2_from(terminalA.a):midpoint(vec2_from(terminalA.b)):into(),
+  vec2_from(terminalA.c):midpoint(vec2_from(terminalA.d)):into()
+};
+
+do
+  local total_gates = 6
+  for i = 1, total_gates do
+    table.insert(terminalA.gates, gate({
+      id        = "A" .. i,
+      pos       = vec2_from(terminalA.apron[1])
+          :lerp(vec2_from(terminalA.apron[2]), i / (total_gates + 1))
+          :move(inverse_degrees(runway22.heading), vec2_from(terminalA.a):distance(vec2_from(terminalA.b)) * 0.35)
+          :into(),
+      heading   = inverse_degrees(runway22.heading),
+      available = false,
+    }))
+  end
+  for i = 1, total_gates do
+    table.insert(terminalA.gates, gate({
+      id        = "A" .. (i + total_gates),
+      pos       = vec2_from(terminalA.apron[1])
+          :lerp(vec2_from(terminalA.apron[2]), i / (total_gates + 1))
+          :move(runway22.heading, vec2_from(terminalA.a):distance(vec2_from(terminalA.b)) * 0.35)
+          :into(),
+      heading   = runway22.heading,
+      available = false,
+    }))
+  end
+end
+
+-- Terminal B
+local terminalB = terminal({
+  id    = "B",
+  a     = vec2_from(taxiwayF4.a):move(runway22.heading, -1):into(),
+  b     = vec2_from(taxiwayF3.a):move(runway22.heading, -1):into(),
+  c     = vec2_from(taxiwayF3.a):move(runway22.heading, TERMINAL_SIZE):into(),
+  d     = vec2_from(taxiwayF4.a):move(runway22.heading, TERMINAL_SIZE):into(),
+  gates = {},
+  apron = { { 0, 0 }, { 0, 0 }, },
+})
+terminalB.apron = {
+  vec2_from(terminalB.a):midpoint(vec2_from(terminalB.b)):into(),
+  vec2_from(terminalB.c):midpoint(vec2_from(terminalB.d)):into()
+};
+
+do
+  local total_gates = 6
+  for i = 1, total_gates do
+    table.insert(terminalB.gates, gate({
+      id        = "B" .. i,
+      pos       = vec2_from(terminalB.apron[1])
+          :lerp(vec2_from(terminalB.apron[2]), i / (total_gates + 1))
+          :move(inverse_degrees(runway13.heading), vec2_from(terminalB.a):distance(vec2_from(terminalB.b)) * 0.35)
+          :into(),
+      heading   = inverse_degrees(runway13.heading),
+      available = false,
+    }))
+  end
+  for i = 1, total_gates do
+    table.insert(terminalB.gates, gate({
+      id        = "B" .. (i + total_gates),
+      pos       = vec2_from(terminalB.apron[1])
+          :lerp(vec2_from(terminalB.apron[2]), i / (total_gates + 1))
+          :move(runway13.heading, vec2_from(terminalB.a):distance(vec2_from(terminalB.b)) * 0.35)
+          :into(),
+      heading   = runway13.heading,
+      available = false,
+    }))
+  end
+end
+
+-- Assemble airport
+local airport = airport({
+  id          = "KDEF",
   frequencies = {
-    approach = 118.5,
-    departure = 118.5,
-    tower = 118.5,
-    ground = 118.5,
-    center = 118.5
+    approach = 118.6,
+    departure = 118.6,
+    tower = 118.6,
+    ground = 118.6,
+    center = 118.7,
   },
-  center = {
-    0.0,
-    0.0
-  },
-  runways = {
-    {
-      id = "13",
-      pos = {
-        1000.0,
-        0.0
-      },
-      heading = 135.0,
-      length = 7200.0
-    },
-    {
-      id = "22",
-      pos = {
-        -1000.0,
-        0.0
-      },
-      heading = 225.0,
-      length = 7200.0
-    }
-  },
-  taxiways = {
-    {
-      id = "A",
-      a = {
-        1262.7417,
-        2828.427
-      },
-      b = {
-        -3828.427,
-        -2262.742
-      }
-    },
-    {
-      id = "B",
-      a = {
-        1828.4272,
-        2262.7417
-      },
-      b = {
-        -3262.7415,
-        -2828.4272
-      }
-    },
-    {
-      id = "C",
-      a = {
-        -1828.4274,
-        2262.7412
-      },
-      b = {
-        3262.7417,
-        -2828.4272
-      }
-    },
-    {
-      id = "D",
-      a = {
-        -1262.742,
-        2828.4268
-      },
-      b = {
-        3828.4272,
-        -2262.7417
-      }
-    },
-    {
-      id = "E1",
-      a = {
-        1121.3203,
-        2828.427
-      },
-      b = {
-        1828.4272,
-        2121.3203
-      }
-    },
-    {
-      id = "E2",
-      a = {
-        -1353.5533,
-        353.5532
-      },
-      b = {
-        -646.4464,
-        -353.55344
-      }
-    },
-    {
-      id = "E3",
-      a = {
-        -2590.99,
-        -883.8837
-      },
-      b = {
-        -1883.883,
-        -1590.9902
-      }
-    },
-    {
-      id = "E4",
-      a = {
-        -3828.427,
-        -2121.3206
-      },
-      b = {
-        -3121.32,
-        -2828.4272
-      }
-    },
-    {
-      id = "F1",
-      a = {
-        -1828.4274,
-        2121.3198
-      },
-      b = {
-        -1121.3206,
-        2828.4268
-      }
-    },
-    {
-      id = "F2",
-      a = {
-        646.4465,
-        -353.5537
-      },
-      b = {
-        1353.5533,
-        353.55322
-      }
-    },
-    {
-      id = "F3",
-      a = {
-        1883.8833,
-        -1590.9905
-      },
-      b = {
-        2590.9902,
-        -883.8835
-      }
-    },
-    {
-      id = "F4",
-      a = {
-        3121.3203,
-        -2828.4272
-      },
-      b = {
-        3828.4272,
-        -2121.3203
-      }
-    }
-  },
-  terminals = {
-    {
-      id = "A",
-      a = {
-        -3192.0308,
-        -2757.7166
-      },
-      b = {
-        -1954.5938,
-        -1520.2795
-      },
-      c = {
-        0.0012207031,
-        -3474.8728
-      },
-      d = {
-        -1237.436,
-        -4712.3105
-      },
-      gates = {
-        {
-          id = "A1",
-          pos = {
-            -1866.0323,
-            -1980.0721
-          },
-          heading = 45.0,
-          available = false
-        },
-        {
-          id = "A2",
-          pos = {
-            -1584.7842,
-            -2261.32
-          },
-          heading = 45.0,
-          available = false
-        },
-        {
-          id = "A3",
-          pos = {
-            -1303.5359,
-            -2542.5679
-          },
-          heading = 45.0,
-          available = false
-        },
-        {
-          id = "A4",
-          pos = {
-            -1022.28784,
-            -2823.816
-          },
-          heading = 45.0,
-          available = false
-        },
-        {
-          id = "A5",
-          pos = {
-            -741.0397,
-            -3105.064
-          },
-          heading = 45.0,
-          available = false
-        },
-        {
-          id = "A6",
-          pos = {
-            -459.79153,
-            -3386.312
-          },
-          heading = 45.0,
-          available = false
-        },
-        {
-          id = "A7",
-          pos = {
-            -2732.2383,
-            -2846.278
-          },
-          heading = 225.0,
-          available = false
-        },
-        {
-          id = "A8",
-          pos = {
-            -2450.99,
-            -3127.5261
-          },
-          heading = 225.0,
-          available = false
-        },
-        {
-          id = "A9",
-          pos = {
-            -2169.7417,
-            -3408.774
-          },
-          heading = 225.0,
-          available = false
-        },
-        {
-          id = "A10",
-          pos = {
-            -1888.4937,
-            -3690.022
-          },
-          heading = 225.0,
-          available = false
-        },
-        {
-          id = "A11",
-          pos = {
-            -1607.2455,
-            -3971.27
-          },
-          heading = 225.0,
-          available = false
-        },
-        {
-          id = "A12",
-          pos = {
-            -1325.9973,
-            -4252.518
-          },
-          heading = 225.0,
-          available = false
-        }
-      },
-      apron = {
-        {
-          -2580.3833,
-          -2131.927
-        },
-        {
-          -611.64636,
-          -4100.663
-        }
-      }
-    },
-    {
-      id = "B",
-      a = {
-        1954.594,
-        -1520.2798
-      },
-      b = {
-        3192.031,
-        -2757.7166
-      },
-      c = {
-        1237.438,
-        -4712.3096
-      },
-      d = {
-        0.0012207031,
-        -3474.8728
-      },
-      gates = {
-        {
-          id = "B1",
-          pos = {
-            1866.0327,
-            -1980.072
-          },
-          heading = 315.0,
-          available = false
-        },
-        {
-          id = "B2",
-          pos = {
-            1584.7849,
-            -2261.32
-          },
-          heading = 315.0,
-          available = false
-        },
-        {
-          id = "B3",
-          pos = {
-            1303.5369,
-            -2542.568
-          },
-          heading = 315.0,
-          available = false
-        },
-        {
-          id = "B4",
-          pos = {
-            1022.2891,
-            -2823.816
-          },
-          heading = 315.0,
-          available = false
-        },
-        {
-          id = "B5",
-          pos = {
-            741.0413,
-            -3105.0637
-          },
-          heading = 315.0,
-          available = false
-        },
-        {
-          id = "B6",
-          pos = {
-            459.7934,
-            -3386.312
-          },
-          heading = 315.0,
-          available = false
-        },
-        {
-          id = "B7",
-          pos = {
-            2732.2388,
-            -2846.2778
-          },
-          heading = 135.0,
-          available = false
-        },
-        {
-          id = "B8",
-          pos = {
-            2450.9907,
-            -3127.526
-          },
-          heading = 135.0,
-          available = false
-        },
-        {
-          id = "B9",
-          pos = {
-            2169.7427,
-            -3408.774
-          },
-          heading = 135.0,
-          available = false
-        },
-        {
-          id = "B10",
-          pos = {
-            1888.495,
-            -3690.0215
-          },
-          heading = 135.0,
-          available = false
-        },
-        {
-          id = "B11",
-          pos = {
-            1607.2472,
-            -3971.2695
-          },
-          heading = 135.0,
-          available = false
-        },
-        {
-          id = "B12",
-          pos = {
-            1325.9993,
-            -4252.5176
-          },
-          heading = 135.0,
-          available = false
-        }
-      },
-      apron = {
-        {
-          2580.3835,
-          -2131.927
-        },
-        {
-          611.64856,
-          -4100.6626
-        }
-      }
-    }
-  }
-}
+  center      = vec2_from(taxiwayE3.a):midpoint(vec2_from(taxiwayF3.b)):into(),
+  runways     = { runway13, runway22 },
+  taxiways    = { taxiwayA, taxiwayB, taxiwayC, taxiwayD,
+    taxiwayE1, taxiwayE2, taxiwayE3, taxiwayE4,
+    taxiwayF1, taxiwayF2, taxiwayF3, taxiwayF4 },
+  terminals   = { terminalA, terminalB },
+})
 
 return airport
