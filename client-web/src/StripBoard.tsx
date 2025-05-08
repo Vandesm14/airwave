e2c3bc3053d09df8b2c4235c33952efe41a3d11b
Shane <vandesm14@gmail.com>
2025-05-08 16:56:24 -0400

    possible (?) stripboard perf fixes

--- client-web/src/StripBoard.tsx
+++ client-web/src/StripBoard.tsx
@@ -166,10 +166,10 @@ function statusOfAircraft(
   return 'None';
 }
 
-type StripProps = {
-  key: unknown;
+type StripProps<T extends HeaderStrip | AircraftStrip> = {
+  key?: unknown;
 
-  strip: Strip;
+  strip: T;
   onmousedown?: () => void;
   onmousemove?: () => void;
   ondelete?: () => void;
@@ -179,8 +179,8 @@ type StripProps = {
   deletable?: boolean;
 };
 
-function Strip(props: StripProps) {
-  const _strip = () => props.strip;
+function HeaderStripView(props: StripProps<HeaderStrip>) {
+  const strip = () => props.strip;
   const onmousedown = () => props.onmousedown;
   const onmousemove = () => props.onmousemove;
   const ondelete = () => props.ondelete;
@@ -190,137 +190,155 @@ function Strip(props: StripProps) {
 
   let input!: HTMLInputElement;
 
+  let [editing, setEditing] = createSignal(false);
+
+  const handleDoubleClick: JSX.EventHandlerUnion<
+    HTMLSpanElement,
+    MouseEvent
+  > = (e) => {
+    e.stopPropagation();
+    setEditing(true);
+
+    if (input) {
+      input.focus();
+    }
+  };
+
+  const handleKeyDown: JSX.EventHandlerUnion<
+    HTMLInputElement,
+    KeyboardEvent
+  > = (e) => {
+    if (e.key === 'Enter') {
+      onedit()?.((e.target as HTMLInputElement).value.trim());
+      setEditing(false);
+    }
+  };
+
+  const handleBlur: JSX.EventHandlerUnion<HTMLInputElement, FocusEvent> = (
+    e
+  ) => {
+    onedit()?.((e.target as HTMLInputElement).value.trim());
+    setEditing(false);
+  };
+
+  const id = () => strip().id;
+  const name = () => strip().name;
+
+  return (
+    <div
+      classList={{
+        strip: true,
+        header: true,
+        dragged: dragged()?.() === id(),
+      }}
+      onmousedown={() => onmousedown()?.()}
+      onmousemove={() => onmousemove()?.()}
+    >
+      <div class="vertical">
+        <Show when={editing()}>
+          <input
+            type="text"
+            onkeydown={handleKeyDown}
+            onblur={handleBlur}
+            value={name()}
+            ref={input}
+          />
+        </Show>
+        <Show when={!editing()}>
+          <span ondblclick={handleDoubleClick}>{name()}</span>
+        </Show>
+      </div>
+      <Show when={deletable()}>
+        <div class="vertical end">
+          <button class="delete" onclick={() => ondelete()?.()}>
+            X
+          </button>
+        </div>
+      </Show>
+    </div>
+  );
+}
+
+function AircraftStripView(props: StripProps<AircraftStrip>) {
+  const strip = () => props.strip;
+  const onmousedown = () => props.onmousedown;
+  const onmousemove = () => props.onmousemove;
+  const ondelete = () => props.ondelete;
+  const dragged = () => props.dragged;
+  const deletable = () => props.deletable;
+
   let [ourFrequency] = useAtom(frequencyAtom);
   let [selectedAircraft, setSelectedAircraft] = useAtom(selectedAircraftAtom);
 
   let [control] = useAtom(controlAtom);
   let [airspace] = useAtom(control().airspace);
-  let [editing, setEditing] = createSignal(false);
 
-  const strip = _strip();
-  if (strip.type === StripType.Header) {
-    const handleDoubleClick: JSX.EventHandlerUnion<
-      HTMLSpanElement,
-      MouseEvent
-    > = (e) => {
-      e.stopPropagation();
-      setEditing(true);
+  const handleMouseDown = () => {
+    setSelectedAircraft(strip().callsign);
+    onmousedown()?.();
+  };
 
-      if (input) {
-        input.focus();
-      }
-    };
+  let dimmer = createMemo(
+    () => strip().frequency !== ourFrequency() || strip().timer.startsWith('-')
+  );
 
-    const handleKeyDown: JSX.EventHandlerUnion<
-      HTMLInputElement,
-      KeyboardEvent
-    > = (e) => {
-      if (e.key === 'Enter') {
-        onedit()?.((e.target as HTMLInputElement).value.trim());
-        setEditing(false);
-      }
-    };
+  const timer = createMemo(() => {
+    return strip().segment === 'parked' || strip().segment === 'boarding'
+      ? strip().timer
+      : smallFlightSegment(strip().segment).toUpperCase();
+  });
 
-    const handleBlur: JSX.EventHandlerUnion<HTMLInputElement, FocusEvent> = (
-      e
-    ) => {
-      onedit()?.((e.target as HTMLInputElement).value.trim());
-      setEditing(false);
-    };
+  const id = () => strip().id;
+  const callsign = () => strip().callsign;
+  const distance = () => strip().distance;
+  const arriving = () => strip().arriving;
+  const departing = () => strip().departing;
+  const topStatus = () => strip().topStatus;
+  const bottomStatus = () => strip().bottomStatus;
+  const frequency = () => strip().frequency;
+  const selected = () => strip().callsign === selectedAircraft();
 
-    return (
-      <div
-        classList={{
-          strip: true,
-          header: true,
-          dragged: dragged()?.() === strip.id,
-        }}
-        onmousedown={() => onmousedown()?.()}
-        onmousemove={() => onmousemove()?.()}
-      >
-        <div class="vertical">
-          <Show when={editing()}>
-            <input
-              type="text"
-              onkeydown={handleKeyDown}
-              onblur={handleBlur}
-              value={strip.name}
-              ref={input}
-            />
-          </Show>
-          <Show when={!editing()}>
-            <span ondblclick={handleDoubleClick}>{strip.name}</span>
-          </Show>
-        </div>
-        <Show when={deletable()}>
-          <div class="vertical end">
-            <button class="delete" onclick={() => ondelete()?.()}>
-              X
-            </button>
-          </div>
-        </Show>
+  return (
+    <div
+      classList={{
+        strip: true,
+        dragged: dragged()?.() === id(),
+        theirs: dimmer(),
+        selected: selected(),
+        departure: airspace() === departing(),
+      }}
+      onmousedown={handleMouseDown}
+      onmousemove={() => onmousemove()?.()}
+    >
+      <div class="vertical">
+        <span class="callsign">{callsign()}</span>
+        <span>{distance()}</span>
       </div>
-    );
-  } else if (strip.type === StripType.Aircraft) {
-    const handleMouseDown = () => {
-      setSelectedAircraft(strip.callsign);
-      onmousedown()?.();
-    };
-
-    let dimmer = createMemo(
-      () => strip.frequency !== ourFrequency() || strip.timer.startsWith('-')
-    );
-
-    const timer = createMemo(() => {
-      return strip.segment === 'parked' || strip.segment === 'boarding'
-        ? strip.timer
-        : smallFlightSegment(strip.segment).toUpperCase();
-    });
-
-    return (
-      <div
-        classList={{
-          strip: true,
-          dragged: dragged()?.() === strip.id,
-          theirs: dimmer(),
-          selected: selectedAircraft() === strip.callsign,
-          departure: airspace() === strip.departing,
-        }}
-        onmousedown={handleMouseDown}
-        onmousemove={() => onmousemove()?.()}
-      >
-        <div class="vertical">
-          <span class="callsign">{strip.callsign}</span>
-          <span>{strip.distance}</span>
-        </div>
-        <div class="vertical">
-          <span>{strip.departing}</span>
-          <span>{strip.arriving}</span>
-        </div>
-        <div class="vertical">
-          <span>{strip.topStatus}</span>
-          <span>{strip.bottomStatus}</span>
-        </div>
-        <div class="vertical">
-          <span class="frequency">{strip.frequency}</span>
-          <span class="timer">{timer()}</span>
-        </div>
-        <Show when={deletable()}>
-          <div class="vertical end">
-            <button
-              class="delete"
-              onclick={() => ondelete()?.()}
-              onmousedown={(e) => e.stopPropagation()}
-            >
-              X
-            </button>
-          </div>
-        </Show>
+      <div class="vertical">
+        <span>{departing()}</span>
+        <span>{arriving()}</span>
       </div>
-    );
-  } else {
-    return null;
-  }
+      <div class="vertical">
+        <span>{topStatus()}</span>
+        <span>{bottomStatus()}</span>
+      </div>
+      <div class="vertical">
+        <span class="frequency">{frequency()}</span>
+        <span class="timer">{timer()}</span>
+      </div>
+      <Show when={deletable()}>
+        <div class="vertical end">
+          <button
+            class="delete"
+            onclick={() => ondelete()?.()}
+            onmousedown={(e) => e.stopPropagation()}
+          >
+            X
+          </button>
+        </div>
+      </Show>
+    </div>
+  );
 }
 
 function aircraftToStrip(
@@ -549,30 +567,32 @@ export default function StripBoard() {
     const selected = selectedAircraft();
 
     if (airspace && aircrafts.data.length > 0) {
-      board.setStrips((strips) =>
-        strips
-          // Update aircraft strips.
-          .map((strip) => {
-            if (strip.type === StripType.Aircraft) {
-              const callsign = strip.callsign;
-              const aircraft = aircrafts.data.find((a) => a.id === callsign);
-              if (aircraft) {
-                return {
-                  ...aircraftToStrip(aircraft, airspace, selected),
-                  id: strip.id,
-                };
-              }
-            }
+      let updateStrips: AircraftStrip[] = [];
+      let deleteStrips: number[] = [];
+      for (const strip of board.strips()) {
+        if (strip.type === StripType.Aircraft) {
+          const callsign = strip.callsign;
+          const aircraft = aircrafts.data.find((a) => a.id === callsign);
+          if (aircraft) {
+            updateStrips.push({
+              ...aircraftToStrip(aircraft, airspace, selected),
+              id: strip.id,
+            });
+          } else {
+            deleteStrips.push(strip.id);
+          }
+        }
+      }
 
-            return strip;
-          })
-          // Filter out aircraft that don't exist.
-          .filter((strip) =>
-            strip.type === StripType.Aircraft
-              ? !!aircrafts.data.find((a) => a.id === strip.callsign)
-              : true
-          )
-      );
+      // Update strips.
+      for (const strip of updateStrips) {
+        board.update(strip, strip.id);
+      }
+
+      // Delete strips.
+      for (const stripId of deleteStrips) {
+        board.remove(stripId);
+      }
     }
   });
 
@@ -696,16 +716,27 @@ export default function StripBoard() {
         {(strip) => {
           return (
             <>
-              <Strip
-                key={strip.id}
-                strip={strip}
-                onmousedown={() => handleMouseDown(strip.id)}
-                onmousemove={() => handleMouseMove(strip.id)}
-                ondelete={() => handleDelete(strip.id)}
-                onedit={(name) => handleEdit(strip.id, name)}
-                dragged={() => (separator() !== null ? dragged() : null)}
-                deletable={strip.id !== INBOX_ID}
-              />
+              {strip.type === StripType.Header ? (
+                <HeaderStripView
+                  strip={strip}
+                  onmousedown={() => handleMouseDown(strip.id)}
+                  onmousemove={() => handleMouseMove(strip.id)}
+                  ondelete={() => handleDelete(strip.id)}
+                  onedit={(name) => handleEdit(strip.id, name)}
+                  dragged={() => (separator() !== null ? dragged() : null)}
+                  deletable={strip.id !== INBOX_ID}
+                />
+              ) : (
+                <AircraftStripView
+                  strip={strip}
+                  onmousedown={() => handleMouseDown(strip.id)}
+                  onmousemove={() => handleMouseMove(strip.id)}
+                  ondelete={() => handleDelete(strip.id)}
+                  onedit={(name) => handleEdit(strip.id, name)}
+                  dragged={() => (separator() !== null ? dragged() : null)}
+                  deletable={strip.id !== INBOX_ID}
+                />
+              )}
               {strip.id === separator() ? <Separator /> : null}
             </>
           );
